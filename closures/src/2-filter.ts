6911a569f1b66f64f82cd35d7c067aa5129ec333
snow6692 <ahmedha258258@gmail.com>
2025-01-22 18:53:18 +0200

    Filter function

--- closures/src/2-filter.ts
+++ closures/src/2-filter.ts
@@ -1,3 +1,43 @@
-type Fn = (n: number, i: number) => any;
+// type Fn = (n: number, i: number) => any;
 
-function filter(arr: number[], fn: Fn): number[] {}
+// function filter(arr: number[], fn: Fn): number[] {
+//   return (function () {
+//     const result: number[] = [];
+
+//     for (const [index, value] of arr.entries()) {
+//       result.push(fn(value, index));
+//     }
+
+//     return result;
+//   })();
+// }
+
+// const greaterThan10 = (n: number, i: number) => n > 10;
+
+// console.log(greaterThan10(10, 1));
+
+// const arr1 = [0, 10, 20, 30];
+// const test = filter(arr1, greaterThan10);
+// console.log(test);
+
+type Fn = (n: number, i: number) => boolean;
+
+function filter(arr: number[], fn: Fn): number[] {
+  return (function () {
+    const result: number[] = [];
+
+    for (const [index, value] of arr.entries()) {
+      if (fn(value, index)) {
+        result.push(value); 
+      }
+    }
+
+    return result;
+  })();
+}
+
+const greaterThan10 = (n: number, i: number) => n > 10;
+
+const arr1 = [0, 10, 20, 30];
+const test = filter(arr1, greaterThan10);
+console.log(test); 
